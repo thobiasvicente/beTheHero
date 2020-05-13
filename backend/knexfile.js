d55c0ba12b433c15b440101a9cbd41706538730d
thobiasvicente <thobiascontact@gmail.com>
2020-05-13 18:40:59 -0300

    just ordering the files

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -7,10 +7,10 @@ module.exports = {
     connection: {
       filename: './src/database/db.sqlite'
     },
-    migrations:{
-      directory:'./src/database/migrations'
+    migrations: {
+      directory: './src/database/migrations'
     },
-    useNullAsDefault: true,
+    useNullAsDefault: true
   },
 
   staging: {
